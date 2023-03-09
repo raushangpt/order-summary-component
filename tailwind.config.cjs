b6a1b8faabe4d1418c29ca6a124247098a6c2d49
raushangpt <rk880488@gmail.com>
2023-03-09 22:59:50 +0530

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.svelte'],
-  theme: {
-    colors: {
-      'blue-100': 'hsl(225, 100%, 98%)',
-      'blue-200': 'hsl(225, 100%, 94%)',
-      'blue-300': 'hsl(224, 23%, 55%)',
-      'blue-400': 'hsl(245, 75%, 52%)',
-      'blue-500': 'hsl(223, 47%, 23%)',
+    content: ['./index.html'],
+    theme: {
+        colors: {
+            'blue-100': 'hsl(225, 100%, 98%)',
+            'blue-200': 'hsl(225, 100%, 94%)',
+            'blue-300': 'hsl(224, 23%, 55%)',
+            'blue-400': 'hsl(245, 75%, 52%)',
+            'blue-500': 'hsl(223, 47%, 23%)',
+        },
+        fontFamily: {
+            'red-hat-display': ['Red Hat Display', 'sans-serif'],
+        },
+        extend: {},
     },
-    fontFamily: {
-      'red-hat-display': ['Red Hat Display', 'sans-serif'],
-    },
-    extend: {},
-  },
-  plugins: [],
+    plugins: [],
 };
